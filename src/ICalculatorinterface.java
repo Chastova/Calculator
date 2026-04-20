7697fab7b2e6478c532ff5162c78662d42200ea1
Chastova <chastova.yekaterina@mail.ru>
2026-04-20 21:04:53 +0300

    Странную ошибку исправила

--- src/ICalculatorinterface.java
+++ src/ICalculatorinterface.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public interface ICalculatorinterface {
+    String add(String a, String b, int base);
+    String subtract(String a, String b, int base);
+    String multiply(String a, String b, int base);
+    String divide(String a, String b, int base);
+    String remainder(String a, String b, int base);
 }
